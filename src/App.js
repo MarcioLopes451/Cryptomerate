eb873fddd4f1984b46b063403113d6c40be1d6ba
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2023-05-05 16:15:41 +0100

    finishing rest of homepage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Navbar from "./Components/Home/Navbar";
-import Banner from "./Components/Home/Banner";
-import PopularCrypto from "./Components/Home/PopularCrypto";
 import Home from "./Components/Home/Home";
 import './App.css'
 
@@ -8,8 +6,6 @@ function App() {
   return (
     <div>
     <Navbar />
-    <Banner />
-    <PopularCrypto />
     <Home />
     </div>
   );
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import PopularCrypto from './PopularCrypto';
+import Banner from './Banner';
 
 export default function Home() {
 
     const mobileImg = 'https://coindeal.com/wp-content/uploads/2020/03/You-have-a-choice.png';
 
   return (
+    <>
+    <Banner />
+    <PopularCrypto />
     <div className='trade-On-The-Go'>
         <div className='trade'>
         <h3>Trade on the go, Anywhere, anytime</h3>
@@ -15,5 +20,6 @@ export default function Home() {
             <h1>Hello</h1>
         </div>
     </div>
+    </>
   )
 }
